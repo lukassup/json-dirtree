405fb2fe8164db116435df831fa91b9f5214ed27
Lukas Šupienis <lukassup@yahoo.com>
2017-08-26 01:53:10 +0300

    feature: display certificate info and find expired certs

--- json_dirtree/__init__.py
+++ json_dirtree/__init__.py
@@ -11,13 +11,28 @@ from __future__ import (
 
 import argparse
 import glob
+import hashlib
 import json
 import logging
 import os
+import shlex
+import subprocess
 import sys
+try:
+    from shutil import which
+except ImportError:
+    from distutils.spawn import find_executable as which
 
 
 BASE_DIR = os.path.dirname(__file__)
+
+
+class JSONDirTreeException(Exception):
+    pass
+
+
+class MissingExecutableError(JSONDirTreeException):
+    pass
 
 
 class CLI(object):
@@ -100,6 +115,39 @@ class CLI(object):
 cli = CLI()
 
 
+def run(command, stdin=None):
+    """Execute a shell command and return the output and stderr)"""
+    if getattr(subprocess.Popen, '__enter__', None) and getattr(subprocess.Popen, '__exit__', None):
+        with subprocess.Popen(
+                shlex.split(command),
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                universal_newlines=True) as proc:
+            try:
+                out, err = proc.communicate(input=stdin, timeout=10)
+            except subprocess.TimeoutExpired:
+                proc.kill()
+                out, err = proc.communicate()
+            finally:
+                rc = proc.returncode
+    else:
+        proc = subprocess.Popen(
+                shlex.split(command),
+                stdin=subprocess.PIPE,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                universal_newlines=True)
+        try:
+            out, err = proc.communicate(input=stdin)
+        except Exception:
+            proc.kill()
+            out, err = proc.communicate()
+        finally:
+            rc = proc.returncode
+    return rc, out, err
+
+
 def dirtree(path, hidden=False, readfiles=False, recursive=False):
     """Build file and directory structure into a dictionary."""
     tree = {}
@@ -126,6 +174,103 @@ def dirtree(path, hidden=False, readfiles=False, recursive=False):
                 # NOTE: only files and directories are handled
                 pass
     return tree
+
+
+def get_openssl_prop(cert_path, prop_name):
+    """Get a single certificate property by its name."""
+    rc, out, _ = run('openssl x509 -noout -%s -in %s' % (prop_name, cert_path))
+    success = rc == 0
+    if not success:
+        logging.warning('unable to get certificate %s for %s', prop_name, cert_path)
+    key, _, value = out.partition('=')
+    return success, key, value.rstrip()
+
+
+def get_openssl_modulus_digest(cert_path, digest_type='sha256'):
+    """Compute digest for OpenSSL certificate key modulus."""
+    rc, out, _ = run('openssl x509 -noout -modulus -in %s' % cert_path)
+    success = rc == 0
+    if not success:
+        logging.warning('unable to get certificate modulus for %s', cert_path)
+        # raise error
+    key, _, value = out.partition('=')
+    hash_func = getattr(hashlib, digest_type)
+    value = value.rstrip().encode()
+    digest = hash_func(value).hexdigest()
+    return success, digest
+
+
+def is_cert_valid(cert_path):
+    """Test if certificate is expired."""
+    rc, out, _ = run('openssl x509 -noout -checkend 0 -in %s' % cert_path)
+    valid = rc == 0
+    return valid
+
+
+def check_certificates(path, hidden=False, recursive=False,
+                       props=('subject', 'issuer', 'startdate', 'enddate', 'serial')):
+    """Check all certificates under a given path."""
+    tree = {}
+    for item in os.listdir(path):
+        # handle files
+        if hidden or not item.startswith('.'):
+            fullpath = os.path.join(path, item)
+            if os.path.isdir(fullpath) and recursive:
+                # recurse into directories
+                tree[item] = check_certificates(fullpath, hidden, recursive, props)
+            elif os.path.isfile(fullpath):
+                # handle certs
+                tree[item] = {}
+                for prop in props:
+                    success, key, value = get_openssl_prop(fullpath, prop)
+                    if not success:
+                        break
+                    tree[item][key] = value
+                if not success:
+                    tree.pop(item, None)
+                    continue
+                # modulus
+                success, digest = get_openssl_modulus_digest(fullpath)
+                tree[item]['modulusSHA256'] = digest
+                # is_expired
+                expired = not is_cert_valid(fullpath)
+                tree[item]['expired'] = expired
+        # NOTE: only files and directories are handled
+    return tree
+
+
+def find_expired_certs(path, hidden=False, recursive=False):
+    tree = {}
+    for item in os.listdir(path):
+        # handle files
+        if hidden or not item.startswith('.'):
+            fullpath = os.path.join(path, item)
+            if os.path.isdir(fullpath) and recursive:
+                # recurse into directories
+                tree[item] = find_expired_certs(fullpath, hidden, recursive)
+            elif os.path.isfile(fullpath):
+                # is_expired
+                tree[item] = not is_cert_valid(fullpath)
+        # NOTE: only files and directories are handled
+    return tree
+
+
+@cli.command('check')
+@cli.option('-x', '--expired', help='find expired certs', action='store_true', default=False)
+@cli.option('-a', '--hidden', help='search hidden files', action='store_true', default=False)
+@cli.option('dirs', nargs='*', help='source directories (default: ./src/* )',
+            default=glob.glob('src/*'))
+def check_certs_command(dirs, hidden, expired):
+    """Verifies TLS certificate validity in directories."""
+    if not which('openssl'):
+        raise MissingExecutableError("executable 'openssl' not found")
+    cli.log.info('verifying TLS certificates in directories: %r', dirs)
+    for d in dirs:
+        if expired:
+            tree = find_expired_certs(d, hidden, recursive=True)
+        else:
+            tree = check_certificates(d, hidden, recursive=True)
+        print(json.dumps(tree, indent=2))
 
 
 @cli.command('build')
